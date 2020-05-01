9d143bdc42c5c6e7e0daa83299660a86a1baaae9
niceprogramming <niceprogramming@github.com>
2020-04-30 20:07:04 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,14 @@
+elli.script.subscribe("loan", "open", onLoanOpen);
+
+async function onLoanOpen(args) {
+    
+    let loan = await elli.script.getObject("loan");    
+    let auth = await elli.script.getObject("auth");
+    
+    let user = await auth.getUser();
+    let payment = await loan.getField("5");
+
+    console.log(user);
+    console.log('Initial payment: ' + validatedPayment);
+    
+}
